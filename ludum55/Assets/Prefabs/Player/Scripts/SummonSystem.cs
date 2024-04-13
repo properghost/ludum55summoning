c663e225598bc44e6fa319ffe069cf7e4abbc105
properghost <bloodhail13@gmail.com>
2024-04-13 20:19:50 +0300

    Update SummonSystem.cs

--- ludum55/Assets/Prefabs/Player/Scripts/SummonSystem.cs
+++ ludum55/Assets/Prefabs/Player/Scripts/SummonSystem.cs
@@ -5,15 +5,26 @@ using UnityEngine;
 
 public class SummonSystem : MonoBehaviour
 {
+    //Rune One
     [SerializeField] private bool runeOne;
+    [SerializeField] private bool alRune;
+    [SerializeField] private bool alRuneUsed;
+    //----------------------------------------
+    //Rune Two
     [SerializeField] private bool runeTwo;
-
+    [SerializeField] private bool gamRune;
+    [SerializeField] private bool gamRuneUsed;
+    //----------------------------------------
     [SerializeField] private bool runeThree;
+    [SerializeField] private bool bazRune;
+    [SerializeField] private bool bazRuneUsed;
+    //----------------------------------------
+    public int runeVal;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        runeVal = 0;
     }
 
     // Update is called once per frame
@@ -22,17 +33,48 @@ public class SummonSystem : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Alpha1))
         {
             runeOne = true;
+            alRune = true;
+            if(alRune && !alRuneUsed)
+            {
+                Invoke("AlRuneActivate", 0);
+                alRuneUsed = true;
+            }
         }
 
         if(Input.GetKeyDown(KeyCode.Alpha2))
         {
-            runeTwo = false;
+            runeTwo = true;
+            gamRune = true;
+            if(gamRune && !gamRuneUsed)
+            {
+                Invoke("GamRuneActivate", 0);
+                gamRuneUsed = true;
+            }
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha3))
         {
             runeThree = true;
+            bazRuneUsed = true;
+            if(bazRune && !bazRuneUsed)
+            {
+                Invoke("BazRuneActivate", 0);
+                bazRuneUsed = true;
+            }
+
         }
-        
+    }
+
+    private void AlRuneActivate()
+    {
+        runeVal += 5;
+    }
+    private void GamRuneActivate()
+    {
+        runeVal *= 2;
+    }
+    private void BazRuneActivate()
+    {
+        runeVal = runeVal * (runeVal / 4);
     }
 }
