1b39cfcec5dacd6d753d1742902d266bb8cfc1d9
properghost <bloodhail13@gmail.com>
2024-04-14 02:24:32 +0300

    Goblin Health Initial Commit

--- ludum55/Assets/Scripts/GoblinAI.cs
+++ ludum55/Assets/Scripts/GoblinAI.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GoblinAI : MonoBehaviour
 {
@@ -12,6 +13,9 @@ public class GoblinAI : MonoBehaviour
     internal bool facingRight;
     internal float distance;
     internal float speed;
+    [SerializeField] private float currentHealth;
+    [SerializeField] private float maxHealth;
+    [SerializeField] private Slider healthSlider;
     
     // Start is called before the first frame update
     void Start()
@@ -20,19 +24,36 @@ public class GoblinAI : MonoBehaviour
         player = GameObject.FindGameObjectWithTag("Knight");
         enemySpeed = 4f;
         distanceBetween = 1;
+        maxHealth = 100f;
+        healthSlider.maxValue = 100;
+        healthSlider.value = maxHealth;
     }
     void Update()
     {
-         if (Vector3.Distance(target.position,transform.position)<20)
-         {
-             transform.position=Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
-             if(target.position.x > transform.position.x && !facingRight) //if the target is to the right of enemy and the enemy is not facing right
-                 Flip();
-             if(target.position.x < transform.position.x && facingRight)
-                 Flip();
-         }
+        healthSlider.value = maxHealth;
+        if (Vector3.Distance(target.position,transform.position)<20)
+        {
+            transform.position=Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+            if(target.position.x > transform.position.x && !facingRight) //if the target is to the right of enemy and the enemy is not facing right
+            Flip();
+            if(target.position.x < transform.position.x && facingRight)
+            Flip();
+        }
+
+        if(currentHealth >= 0f)
+        {
+            Destroy(gameObject, 0.01f);
+        }
+
     }
 
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Knight")
+        {
+            currentHealth -= 1f;
+        }
+    }
    
     void FixedUpdate()
     {
@@ -54,4 +75,6 @@ public class GoblinAI : MonoBehaviour
          transform.localScale = scale;
          facingRight = !facingRight;
     }
+
+    
 }
