cbca46e435189e3a2f68104a5562e30a9ff19b8a
Asrata012 <asrata012@gmail.com>
2024-04-13 20:55:28 +0300

    Update AIChase.cs
    enemy ai

--- ludum55/Assets/Scripts/AIChase.cs
+++ ludum55/Assets/Scripts/AIChase.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class AIChase : MonoBehaviour
 {
+    public GameObject player;
+    public float enemySpeed;
+    private float distance;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,9 @@ public class AIChase : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        distance = Vector2.Distance(transform.position, player.transform.position);
+        Vector2 direction = player.transform.position - transform.position;
+
+        transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, enemySpeed * Time.deltaTime);
     }
 }
