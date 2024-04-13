467808f499ec8c2c7cf90d96c7ac10790dd088a6
Asrata012 <asrata012@gmail.com>
2024-04-13 21:49:58 +0300

    AIChase
    Semi-final update

--- ludum55/Assets/Scripts/AIChase.cs
+++ ludum55/Assets/Scripts/AIChase.cs
@@ -8,16 +8,29 @@ public class AIChase : MonoBehaviour
     public GameObject player;
     public float enemySpeed;
     public float distanceBetween;
-    
+    [SerializeField] private Transform target;
+    private bool facingRight;
     private float distance;
+    private float speed;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = GameObject.Find("Player").transform;
+    }
+    void Update()
+    {
+         if (Vector3.Distance(target.position,transform.position)<20)
+         {
+             transform.position=Vector2.MoveTowards(transform.position, target.position,speed * Time.deltaTime);
+             if(target.position.x > transform.position.x && !facingRight) //if the target is to the right of enemy and the enemy is not facing right
+                 Flip();
+             if(target.position.x < transform.position.x && facingRight)
+                 Flip();
+         }
     }
 
-    // Update is called once per frame
+   
     void FixedUpdate()
     {
         distance = Vector2.Distance(transform.position, player.transform.position);
@@ -30,5 +43,12 @@ public class AIChase : MonoBehaviour
         {
         transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, enemySpeed * Time.deltaTime);
         }
+    }
+    void Flip()
+    {
+         Vector3 scale = transform.localScale;
+         scale.x *= -1;
+         transform.localScale = scale;
+         facingRight = !facingRight;
     }
 }
