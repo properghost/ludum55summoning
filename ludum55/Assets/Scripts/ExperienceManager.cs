2165d78980e710aba2ac40e1fe3752b7fde69c4c
Asrata012 <asrata012@gmail.com>
2024-04-14 17:22:36 +0300

    Exp

---
+++ ludum55/Assets/Scripts/ExperienceManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExperienceManager : MonoBehaviour
+{
+   public static ExperienceManager Instance;
+
+   private void Awake()
+   {
+    if (Instance != null && Instance != this)
+   }
+}
