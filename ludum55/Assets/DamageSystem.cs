87a24d17efcca52ff09cada1293892b1fc778109
properghost <bloodhail13@gmail.com>
2024-04-14 17:21:31 +0300

    mid patch

---
+++ ludum55/Assets/DamageSystem.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageSystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void TakeDamage(float damage)
+    {
+        
+    }
+}
